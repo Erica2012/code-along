731c203e02560c6c259bcaa686324b940f5eecff
Erica2012 <mcrenoldj@gmail.com>
2022-08-16 07:20:48 -0700

    controlled Form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import Header from "./components/Header";
-import Button from "./components/Button";
-import { Image } from "./components/Image";
-import Navbar from "./components/Navbar";
+// import Header from "./components/Header";
+// import Button from "./components/Button";
+// import { Image } from "./components/Image";
+// import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 
 function App(){
 return (
     <div> 
-        <Navbar />
+        {/* <Navbar />
         <Header />;
         <Button label="Add to Cart" />
         <Button label="Read more..." />
         <Button label="Register" />
         <Button label="Login" />
-        <Image />
+        <Image /> */
+        <Form />
+        }
     </div>
 )
 }
